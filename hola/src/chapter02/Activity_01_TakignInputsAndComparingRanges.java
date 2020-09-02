8a061883f01fd1773ab55b395f48e4340b16a5db
Crhistian <matrixcristian21@gmail.com>
2020-09-02 01:20:06 -0500

    activity 1

---
+++ hola/src/chapter02/Activity_01_TakignInputsAndComparingRanges.java
@@ -0,0 +1,52 @@
+package chapter02;
+
+public class Activity_01_TakignInputsAndComparingRanges {
+    public static void main(String[] args){
+        //Systole / diastole command-line input
+        int systole= Integer.parseInt(args[0]);
+        int diastole = Integer.parseInt(args[1]);
+
+        //if the number or arguments doesn't match
+        while ((args.length != 2)){
+            System.err.println("\nInput error. Please introduce the input data this way: systole_value diastole_value");
+            break;
+        }
+        if (args.length == 2){
+            //Program info
+
+            //systole
+            if ((systole < 90) && (systole > 0)){
+                System.out.println("\n-Low systolic pressure");
+            }
+            if ((systole >= 90) && (systole <= 120)){
+                System.out.println("\n-Ideal systolic pressure");
+            }
+            if ((systole > 120) && (systole <= 140)){
+                System.out.println("\n-Pre-high systolic pressure");
+            }
+            if (systole > 140){
+                System.out.println("\n-High systolic pressure");
+            }
+
+            //diastole
+            if ((diastole < 60) && (diastole > 0)){
+                System.out.println("-Low diastolic pressure");
+            }
+            if ((diastole >= 60) && (diastole <= 80)){
+                System.out.println("-Ideal diastolic pressure");
+            }
+            if ((diastole > 80) && (diastole <= 90)){
+                System.out.println("-Pre-high diastolic pressure");
+            }
+            if (diastole > 90){
+                System.out.println("-High diastolic pressure");
+            }
+            else
+                System.out.println("=> You are not live son :v");
+
+            //report
+            System.out.println(systole + "/" + diastole + "\n");
+
+        }
+    }
+}
